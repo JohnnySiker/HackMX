d530661a7fc2364b4f027a7b4df1de233c08fe3b
Jonathan Velazquez <jonhysiker@icloud.om>
2015-03-07 04:57:11 -0600

    SingUp

---
+++ Estacionate/Bridge.h
@@ -0,0 +1,13 @@
+//
+//  Bridge.h
+//  Estacionate
+//
+//  Created by Israel Velazquez Sanchez on 07/03/15.
+//  Copyright (c) 2015 Clipp. All rights reserved.
+//
+
+#ifndef Estacionate_Bridge_h
+#define Estacionate_Bridge_h
+
+#import <FacebookSDK/FacebookSDK.h>
+#endif
--- Estacionate/ViewController.swift
+++ Estacionate/ViewController.swift
@@ -8,16 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,FBLoginViewDelegate {
 
+    @IBOutlet weak var fb_icon: FBLoginView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        fb_icon.delegate = self
+        fb_icon.readPermissions = ["public_profile", "email", "user_friends"]
+        
     }
+    override func viewWillAppear(animated: Bool) {
+        self.navigationController?.navigationBar.alpha = 0
+    }
+    
+    
+    func loginViewFetchedUserInfo(loginView : FBLoginView!, user: FBGraphUser) {
+        println("User: \(user)")
+        println("User ID: \(user.objectID)")
+        println("User Name: \(user.name)")
+        var userEmail = user.objectForKey("email") as String
+        var userGender = user.objectForKey("gender") as String
+        println("User Email: \(userEmail)")
+        
+        println("Genero del usuario: \(userGender)")
+    }
+    
+    func loginViewShowingLoggedOutUser(loginView : FBLoginView!) {
+        println("User Logged Out")
+    }
+    
+    func loginView(loginView : FBLoginView!, handleError:NSError) {
+        println("Error: \(handleError.localizedDescription)")
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func SignIn(sender: UIButton) {
+        let nView = self.storyboard?.instantiateViewControllerWithIdentifier("SignUp") as SignUp
+        self.navigationController?.pushViewController(nView, animated: true)
+        
+    }
+    
+    
+    @IBAction func LogIn(sender: UIButton) {
+       // let nView = self.storyboard?.instantiateViewControllerWithIdentifier("LogIn") as LogIn
+       // self.navigationController?.pushViewController(nView, animated: true)
+        
     }
 
 
