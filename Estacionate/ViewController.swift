2910fe7e1d464cd6332b91d5580850252d651f13
Jonathan Velazquez <jonhysiker@icloud.om>
2015-03-07 03:16:40 -0600

    Proyecto inicial

---
+++ Estacionate/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Estacionate
+//
+//  Created by Israel Velazquez Sanchez on 07/03/15.
+//  Copyright (c) 2015 Clipp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
